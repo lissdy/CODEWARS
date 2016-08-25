e2f3beb46046df471981df8ffea002597469707c
lisa <lissdy2013@gmail.com>
2016-08-25 18:18:51 +0800

    Nesting_Structure_Comparison.rb

--- Nesting_Structure_Comparison.rb
+++ Nesting_Structure_Comparison.rb
@@ -22,6 +22,32 @@ class Array
 end
 
 
+class Array
+  def same_structure_as(other)
+    # 把数组全部转换成字符串
+    self_str = self.to_s
+    other_str = other.to_s
+    # 如果参比的other不是数组或者是数组但是长度不同则返回false
+    return false if other.class != Array || self.size != other.size
+    # 两个括号
+     two_brackets = %w([ ])
+
+     # 字符串模式匹配 从0开始到结束
+    0.upto(self_str.size-1) do |i|
+      # 如果元素是括号
+      if two_brackets.include?(self_str[i])
+        # 参比元素必须是相同的括号 否则return false
+         return false  if self_str[i] != other_str[i]
+      else
+        # 如果元素不是括号 则判断参比元素不能是括号否则return false
+          return false if two_brackets.include?(other_str[i])
+      end
+    end
+
+    true
+  end
+end
+
 #BEST PRACTICE
 class Array
   def same_structure_as(a)
